312d720267bb0e923ac425a1e2501e20033545b0
nenekelvin <karvinmitchar@gmail.com>
2019-10-17 12:54:40 +0300

    info page on layout page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/info'
+  
   resources :casts
 root to: redirect('/casts')  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
